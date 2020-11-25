7bcbc002ecb09438854e1ce0726295e8cc1f3676
Aun Abbas <imaunabbas@gmail.com>
2020-11-25 16:23:54 +0500

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import { LEADERS } from '../shared/leaders';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = (state) => {
+  return {
+    leaders: state.leaders,
+  };
+};
 
 class About extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      leaders: LEADERS,
-    };
-  }
-
   static navigationOptions = {
     title: 'About Us',
   };
 
   render() {
-    const renderLeaders = ({ item, index }) => {
+    const renderLeader = ({ item, index }) => {
       return (
         <ListItem
           key={index}
@@ -24,7 +24,7 @@ class About extends Component {
           titleStyle={{ color: 'black' }}
           subtitle={item.description}
           hideChevron={true}
-          leftAvatar={{ source: require('./images/alberto.png') }}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
         />
       );
     };
@@ -54,8 +54,8 @@ class About extends Component {
         <History />
         <Card title='Corporate Leadership'>
           <FlatList
-            data={this.state.leaders}
-            renderItem={renderLeaders}
+            data={this.props.leaders.leaders}
+            renderItem={renderLeader}
             keyExtractor={(item) => item.id.toString()}
           />
         </Card>
@@ -64,4 +64,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -19,6 +19,29 @@ import Home from './HomeComponent';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import { Icon } from 'react-native-elements';
+import { connect } from 'react-redux';
+import {
+  fetchDishes,
+  fetchComments,
+  fetchPromos,
+  fetchLeaders,
+} from '../redux/ActionCreators';
+
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchDishes: () => dispatch(fetchDishes()),
+  fetchComments: () => dispatch(fetchComments()),
+  fetchPromos: () => dispatch(fetchPromos()),
+  fetchLeaders: () => dispatch(fetchLeaders()),
+});
 
 const MenuNavigator = createStackNavigator(
   {
@@ -210,6 +233,13 @@ const MainNavigator = createDrawerNavigator(
 );
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchDishes();
+    this.props.fetchComments();
+    this.props.fetchPromos();
+    this.props.fetchLeaders();
+  }
+
   render() {
     return (
       <View
@@ -249,4 +279,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
