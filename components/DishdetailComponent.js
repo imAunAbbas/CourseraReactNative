6a55f11ef056d9541a7a462ede5affdecbdd87ed
Aun Abbas <imaunabbas@gmail.com>
2020-11-27 15:35:12 +0500

    Assignment 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
-import { Card, Icon } from 'react-native-elements';
+import {
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Modal,
+  Button,
+  StyleSheet,
+} from 'react-native';
+import { Card, Icon, Rating, Input } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
   return {
@@ -15,25 +23,35 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => ({
   postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+  postComment: (dishId, rating, author, comment) =>
+    dispatch(postComment(dishId, rating, author, comment)),
 });
 
-function RenderDish(props) {
-  const dish = props.dish;
-
+function RenderDish({ dish, favorite, markFavorite, openCommentForm }) {
   if (dish != null) {
     return (
       <Card featuredTitle={dish.name} image={{ uri: baseUrl + dish.image }}>
         <Text style={{ margin: 10 }}>{dish.description}</Text>
-        <Icon
-          raised
-          reverse
-          name={props.favorite ? 'heart' : 'heart-o'}
-          type='font-awesome'
-          color='#f50'
-          onPress={() =>
-            props.favorite ? console.log('Already favorite') : props.onPress()
-          }
-        />
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+          <Icon
+            raised
+            reverse
+            name={favorite ? 'heart' : 'heart-o'}
+            type='font-awesome'
+            color='#f50'
+            onPress={() =>
+              favorite ? console.log('Already favorited') : markFavorite()
+            }
+          />
+          <Icon
+            raised
+            reverse
+            name='pencil'
+            type='font-awesome'
+            color='#512DA8'
+            onPress={() => openCommentForm()}
+          />
+        </View>
       </Card>
     );
   } else {
@@ -41,9 +59,7 @@ function RenderDish(props) {
   }
 }
 
-function RenderComments(props) {
-  const comments = props.comments;
-
+function RenderComments({ comments }) {
   const renderCommentItem = ({ item, index }) => {
     return (
       <View key={index} style={{ margin: 10 }}>
@@ -72,8 +88,52 @@ class Dishdetail extends Component {
     title: 'Dish Details',
   };
 
+  constructor(props) {
+    super(props);
+    this.state = this.defaultState();
+  }
+
+  defaultState() {
+    return {
+      rating: 5,
+      author: '',
+      comment: '',
+      showCommentForm: false,
+    };
+  }
+
   markFavorite(dishId) {
     this.props.postFavorite(dishId);
+  }
+
+  resetCommentForm() {
+    this.setState(this.defaultState());
+  }
+
+  handleComment(dishId) {
+    this.props.postComment(
+      dishId,
+      this.state.rating,
+      this.state.author,
+      this.state.comment
+    );
+    this.resetCommentForm();
+  }
+
+  openCommentForm() {
+    this.setState({ showCommentForm: true });
+  }
+
+  setRating(rating) {
+    this.setState({ rating });
+  }
+
+  setAuthor(author) {
+    this.setState({ author });
+  }
+
+  setComment(comment) {
+    this.setState({ comment });
   }
 
   render() {
@@ -84,15 +144,103 @@ class Dishdetail extends Component {
           dish={this.props.dishes.dishes[+dishId]}
           favorite={this.props.favorites.some((el) => el === dishId)}
           onPress={() => this.markFavorite(dishId)}
+          markFavorite={() => this.markFavorite(dishId)}
+          openCommentForm={() => this.openCommentForm()}
         />
         <RenderComments
           comments={this.props.comments.comments.filter(
             (comment) => comment.dishId === dishId
           )}
         />
+        <Modal
+          animationType={'slide'}
+          transparent={false}
+          visible={this.state.showCommentForm}
+          onDismiss={() => {
+            this.resetCommentForm();
+          }}
+          onRequestClose={() => {
+            this.resetCommentForm();
+          }}
+        >
+          <View style={styles.modal}>
+            <Rating
+              minValue={1}
+              startingValue={5}
+              fractions={0}
+              showRating={true}
+              onFinishRating={(rating) => this.setRating(rating)}
+            />
+            <Input
+              placeholder='Author'
+              leftIconContainerStyle={{ marginRight: 10 }}
+              leftIcon={<Icon name='user-o' type='font-awesome' />}
+              onChangeText={(author) => this.setAuthor(author)}
+            />
+            <Input
+              placeholder='Comment'
+              leftIconContainerStyle={{ marginRight: 10 }}
+              leftIcon={<Icon name='comment-o' type='font-awesome' />}
+              onChangeText={(comment) => this.setComment(comment)}
+            />
+            <View
+              style={{
+                marginVertical: 10,
+              }}
+            >
+              <Button
+                title='Submit'
+                color='#512DA8'
+                onPress={() => {
+                  this.handleComment(dishId);
+                }}
+              />
+              <Button
+                title='Cancel'
+                color='#6c757d'
+                onPress={() => {
+                  this.resetCommentForm();
+                }}
+              />
+            </View>
+          </View>
+        </Modal>
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  formRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    flexDirection: 'row',
+    margin: 20,
+  },
+  formLabel: {
+    fontSize: 18,
+    flex: 2,
+  },
+  formItem: {
+    flex: 1,
+  },
+  modal: {
+    justifyContent: 'center',
+    margin: 20,
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    backgroundColor: '#512DA8',
+    textAlign: 'center',
+    color: 'white',
+    marginBottom: 20,
+  },
+  modalText: {
+    fontSize: 18,
+    margin: 10,
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
